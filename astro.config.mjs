fa7aa743308277f33c17b75527d50494afcf79a0
tomohikohorai-source <tomohiko.horai@gmail.com>
2026-01-12 16:01:51 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'hybrid', // ページは静的、ログインはサーバーで処理
-  adapter: vercel(),
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
