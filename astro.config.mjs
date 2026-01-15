3c88964ab27bd63301e9bd8e045963035445f5bf
tomohikohorai-source <tomohiko.horai@gmail.com>
2026-01-15 20:42:23 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://penan-japanese-news.vercel.app',
   output: 'static',
+  integrations: [], // 一旦エラー回避のため空にします
 });
