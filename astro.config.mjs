ff2f6d6724186f16d3a44306cf64266865cc9fb9
tomohikohorai-source <tomohiko.horai@gmail.com>
2026-01-15 19:16:12 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  // あなたの実際のURLに書き換えてください
+  site: 'https://penan-japanese-news.vercel.app',
+  integrations: [sitemap()],
   output: 'static',
 });
